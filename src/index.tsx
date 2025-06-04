f93020e9021ac2f6c7d45e33a3767c54676ce480
ThaoDTT <thanhthao97.qng@gmail.com>
2025-06-04 17:24:08 +0700

    update import { HashRouter as Router } from 'react-router-dom'; trong index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Sử dụng HashRouter thay cho BrowserRouter */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
+
 reportWebVitals();
